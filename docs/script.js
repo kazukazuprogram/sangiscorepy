2d74c01859c5c98852d2fa903d6686a5b90610ea
kazukazuprogram <cabbage.lettuce.wireless@gmail.com>
2019-02-27 22:08:57 +0900

    A

--- docs/script.js
+++ docs/script.js
@@ -4,63 +4,39 @@ function errormsg(msg,n) {
 
 function ch() {
   var form = document.getElementById('form');
-  var erroca = false;
-  for (x=0;x<9;x+=1) {
-    if (form[x].value>5) {
-      errormsg("調査書欄に入力ミスがあります","1");
-      erroca=true;
-    }
-  }
-  if (!erroca) {
-    errormsg("","1");
-  }
-  var errocb = false;
-  for (x=9;x<12;x+=1) {
-    if (form[x].value>100) {
-      errormsg("学力検査欄に入力ミスがあります","2");
-      errocb=true;
-    }
-  }
-  if (!errocb) {
-    errormsg("","2");
-  } else {
-    return false;
-  }
-  if (erroca) {
-    return false;
-  }
   var nb=0;
   for (var x=0;x<9;x+=1){
     if (form[x].value=="") {
       nb += 0;
       console.log("空欄:",x, form[x].value);
+    } else if (x==1) {
+      nb += eval(form[x].value)*1.2; // 社会
     } else if (x==3) {
-      nb += eval(form[x].value)*1.2;
-    } else if (x==4) {
-      nb += eval(form[x].value)*1.4;
-    } else if (x>5) {
-      nb += eval(form[x].value)*1.2;
+      nb += eval(form[x].value)*1.4; // 理科
+    } else if (x>4) {
+      nb += eval(form[x].value)*1.2; // 実技科目
     } else {
-      nb += eval(form[x].value);
+      nb += eval(form[x].value); // その他
     }
   }
-  var naa = Math.floor(nb);
-  var na = naa * 300 / 52;
-  var naishin = Math.floor(na);
+  var naa = Math.floor(nb); // 小数点切り捨て
+  var na = naa * 300 / 52; // 300点満点に換算
+  var naishin = Math.floor(na); // 小数点切り捨て
   var exam = 0;
   for (x=9;x<12;x+=1) {
     if (form[x].value=="") {
       exam += 0;
       // console.log("空欄",x, form[x].value);
+    } else if (x==10) {
+      exam +=form[x].value*1.5;
     } else {
-      exam += eval(form[x].value)*1.2;
+      exam += eval(form[x].value);
     }
   }
   exam = Math.floor(exam)*2;
-  // console.log(nb, na, naishin);
   var score = naishin + exam;
-  document.getElementById("naishin").innerHTML = document.getElementById("naishin").innerHTML.replace("{0}",naishin).replace("{3}",naa);
-  document.getElementById("exam").innerHTML = document.getElementById("exam").innerHTML.replace("{1}",exam);
+  document.getElementById("naishin_result").innerHTML = "換算内申&nbsp;&nbsp;{3}/52点&nbsp;&nbsp;{0}/300点".replace("{0}",naishin).replace("{3}",naa);
+  document.getElementById("exam_result").innerHTML = "学力検査&nbsp;&nbsp;{1}/700点".replace("{1}",exam);
   var writestr = "<div><h3>調査書</h3><font size=\"8\">{0}</font>/300点</div><div><h3>学力検査</h3><font size=\"8\">{1}</font>/700点</div><div><h3>合計</h3><font size=\"8\">{2}</font>/1000点</div>".replace("{0}",naishin).replace("{1}",exam).replace("{2}",score).replace("{3}",naa);
   document.getElementById("result").innerHTML = writestr;
 
